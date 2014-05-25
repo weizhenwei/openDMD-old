a740a1f9b2c57a3b29c7e4e6439aeaed45562c84
weizhenwei <weizhenwei-hf@loongson.cn>
2014-05-25 22:31:09 +0800

    add daemonize utility to project

--- src/main.c
+++ src/main.c
@@ -39,28 +39,29 @@
  * *****************************************************************************
  */
 
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <locale.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/resource.h>
+
 #include "dmd_log.h"
 #include "dmd_video.h"
 #include "dmd_signal.h"
 #include "dmd_v4l2_utils.h"
 #include "dmd_image_capture.h"
 
+#define PIDFILE "/home/wzw/openDMD/opendmd.pid"
+
 extern struct v4l2_device_info *dmd_video;
 
 extern time_t lasttime;
 extern unsigned short int counter_in_minute;
 
-void init(void)
-{
-    // signal init;
-    signal_init();
-
-    lasttime = time(&lasttime);
-    counter_in_minute = 0;
-
-    dmd_openlog(DMD_IDENT, DMD_LOGOPT, DMD_FACILITY);
-}
 
 void clean(void)
 {
@@ -94,6 +95,105 @@ void working_progress()
     assert(ret != -1);
 
     dmd_video_release(dmd_video);
+}
+
+void daemonize()
+{
+    pid_t pid;
+    int i, fd0, fd1, fd2;
+    struct rlimit r1;
+    struct sigaction sa;
+    FILE *fp = NULL;
+
+    // clear file creation mask.
+    umask(0);
+
+    // get maximum numbers of file descriptors.
+    if (getrlimit(RLIMIT_NOFILE, &r1) == -1) {
+        dmd_log(LOG_ERR, "getrlimit-file limit error.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // fork and parent exits.
+    if ((pid = fork()) == -1) {
+        dmd_log(LOG_ERR, "fork error.\n");
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) { // parent
+        exit(EXIT_SUCCESS);
+    }
+
+    // the folloing is runing by child.
+
+    // become a session leader to lose controlling  TTY.
+    if ((pid = setsid()) == -1) {
+        dmd_log(LOG_ERR, "setsid error.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // set SIGHUP sighandler;
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    if (sigaction(SIGHUP, &sa, NULL) == -1) {
+        dmd_log(LOG_ERR, "sigaction error.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // fork again, the grandchild alive since then.
+    if ((pid = fork()) == -1) {
+        dmd_log(LOG_ERR, "fork 2 error.\n");
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) { // parent
+        exit(EXIT_SUCCESS);
+    }
+
+    // change the current working directory to the root
+    // so we won't prevent file systems from being unmounted.
+    if (chdir("/") == -1) {
+        dmd_log(LOG_ERR, "chdir to root error.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // close all open file descriptors.
+    if (r1.rlim_max == RLIM_INFINITY) {
+        r1.rlim_max = 1024;
+    }
+    for (i = 0; i < r1.rlim_max; i++) {
+        close(i);
+    }
+
+    // attach file descriptors 0, 1, and 2 to /dev/null.
+    fd0 = open("/dev/null", O_RDWR);
+    fd1 = dup(0);
+    fd2 = dup(0);
+    if (fd0 != 0 || fd1 != 1 || fd2 != 2) {
+        dmd_log(LOG_ERR, "unexpected file descriptors: %d, %d, %d\n",
+                fd0, fd1, fd2);
+        exit(EXIT_FAILURE);
+    }
+
+    // write pid to file
+    pid = getpid();
+    if ((fp = fopen(PIDFILE, "w")) == NULL) {
+        dmd_log(LOG_ERR, "fopen PIDFILE error.\n");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(fp, "%d", pid);
+    fclose(fp);
+
+}
+
+void init(void)
+{
+    // signal init;
+    signal_init();
+
+    lasttime = time(&lasttime);
+    counter_in_minute = 0;
+
+    dmd_openlog(DMD_IDENT, DMD_LOGOPT, DMD_FACILITY);
+
+    daemonize();
 }
 
 int main(int argc, char *argv[])
