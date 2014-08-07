1fd8da3b04abe9e5f4b0b9d81a99ab656c395156
weizhenwei <weizhenwei-hf@loongson.cn>
2014-08-07 20:14:12 +0800

    add some tiny fix up

--- total_lines.pl
+++ total_lines.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl 
 
 my $headerline= `find ./src -name \*.h | xargs cat | wc -l`;
-print "headerline = ".$headerline;
+print "headerline in ./src dir = ".$headerline;
 
 my $sourceline= `find ./src -name \*.c | xargs cat | wc -l`;
-print "sourceline = ".$sourceline;
+print "sourceline in ./src dir = ".$sourceline;
 
 my $totalline = $headerline + $sourceline;
-print "totalline  = ".$totalline;
+print "totalline  in ./src dir = ".$totalline;
 print "\n";
