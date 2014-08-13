dd87fdb4b77f1aa0463c1eccddfc1f433114ff59
weizhenwei <weizhenwei-hf@loongson.cn>
2014-08-13 20:56:20 +0800

    modify line counting script

--- total_lines.pl
+++ total_lines.pl
@@ -1,11 +1,16 @@
 #!/usr/bin/perl 
 
 my $headerline= `find ./src -name \*.h | xargs cat | wc -l`;
-print "headerline in ./src dir = ".$headerline;
+print "header line in ./src dir = ".$headerline;
 
 my $sourceline= `find ./src -name \*.c | xargs cat | wc -l`;
-print "sourceline in ./src dir = ".$sourceline;
+print "source line in ./src dir = ".$sourceline;
 
-my $totalline = $headerline + $sourceline;
+my $testhdrline = `find ./libortp/test -name \*.h | xargs cat | wc -l`;
+print "header line in ./libortp/test dir = ".$testhdrline;
+my $testsrcline = `find ./libortp/test -name \*.c | xargs cat | wc -l`;
+print "source line in ./libortp/test dir = ".$testsrcline;
+
+my $totalline = $headerline + $sourceline + $testhdrline + $testsrcline;
 print "totalline  in ./src dir = ".$totalline;
 print "\n";
